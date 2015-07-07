d103889a352d2f8749be041aa8ad997f91f42c85
cjmarvin <chris.j.marvin@gmail.com>
2015-07-07 22:30:47 +0200

    update bashprompt.sh

--- bash/bashprompt.sh
+++ bash/bashprompt.sh
@@ -13,26 +13,27 @@ set_prompt () {
     nfiles="\$(ls -1 | wc -l | sed 's: ::g') files"
     nsize="\$(ls -skh | head -n1 | sed 's/total //')b"
     GIT_PS1_SHOWDIRTYSTATE=1
-    GIT_PS1_SHOWUNTRACKEDFILES=1
-    GIT_PS1_SHOWUPSTREAM="verbose"
-    GIT_PS1_SHOWCOLORHINTS=1
+    GIT_PS1_SHOWSTASHSTATE=1
+    GIT_PS1_SHOWUNTRACKEDFILES=
+    GIT_PS1_SHOWUPSTREAM="auto"
+    GIT_PS1_SHOWCOLORHINTS=
 
     if [[ -n $SSH_CONNECTION ]]; then
-        PS1="\n$Red(ssh) "
-        PS1+="$IGreen\u"
-        PS1+="$Grey@"
-        Ps1+="$Purple\h "
+        PS1="\n${Red}[ssh] "
+        PS1+="${IGreen}\u"
+        PS1+="${Grey}@"
+        Ps1+="${Purple}\h "
     else
-        PS1="\n$IGreen\u "
+        PS1="\n${IGreen}\u "
     fi
-    PS1+="($IGreen\j jobs$Grey) ($IGreen\@ \d$Grey)\n"
-    PS1+="$Blue\w$Grey$Red\$(__git_ps1)$Grey\n"
-    PS1+="$Grey("
-    if [[ $Last_Command == 0 ]]; then
-        PS1+="$Green$Checkmark"
+    PS1+="(${IGreen}\j jobs${Grey}) (${IGreen}\@ \d${Grey})\n"
+    PS1+="${Blue}\w${Grey}${Red}\$(__git_ps1)${Grey}\n"
+    PS1+="${Grey}("
+    if [[ ${Last_Command} == 0 ]]; then
+        PS1+="${Green}${Checkmark}"
     else
-        PS1+="$Red$FancyX"
+        PS1+="${Red}${FancyX}"
     fi
-    PS1+="$Grey) "
-    PS1+="$IGreen\$ $Reset"
+    PS1+="${Grey}) "
+    PS1+="${IGreen}\$ ${Reset}"
     }
