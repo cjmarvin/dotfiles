038a6bade86150ac08907cb8bd95d0733fa8f5e6
cjmarvin <chris.j.marvin@gmail.com>
2015-08-26 15:49:59 +0200

    initial commit
    This small shell script just initializes the necessary dotfiles.

---
+++ link.sh
@@ -0,0 +1,19 @@
+# GIT
+echo '[user]' > ~/.gitconfig.local
+echo '    name = ' >> ~/.gitconfig.local
+echo '    email = ' >> ~/.gitconfig.local
+vim ~/.gitconfig.local
+#ln -sv `pwd`/git/gitconfig ~/.gitconfig
+# ln -sv `pwd`/git/gitignore ~/.gitignore
+
+# TMUX
+# ln -sv `pwd`/tmux/tmux.conf ~/.tmux.conf
+
+# VIM
+# ln -sv `pwd`/vim/vim ~/.vim
+# ln -sv `pwd`/vim/vimrc ~/.vimrc
+
+# ZSH
+# ln -sv `pwd`/zsh/.zshenv ~/.zshenv
+# ln -sv `pwd`/zsh ~/.zshrc
+# ln -sv `pwd`/zsh/zshrc ~/.zshrc
